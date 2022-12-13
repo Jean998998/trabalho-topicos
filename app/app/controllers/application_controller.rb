82b2e379bb3ce751264b924273b401689f35e11b
jean <jean.sodre@siedos.com.br>
2022-12-13 10:12:43 -0300

    Correcting Devise problem, second try

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  skip_before_action :verify_authenticity_token, :only => :create
+  protect_from_forgery with: :null_session
   before_action :set_locale
   helper_method :current_user
   helper_method :logged_in?
